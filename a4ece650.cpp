cd9121147084b2c121492b6261087cd7b50f64ee
Manav Dhindsa <40146645+manavdhindsa99@users.noreply.github.com>
2023-04-09 04:12:46 -0400

    Update a4ece650.cpp
    code working with timeout functions properly

--- a4ece650.cpp
+++ a4ece650.cpp
@@ -9,6 +9,7 @@
 #include <climits>
 #include <bits/stdc++.h>
 #include <pthread.h>
+#include <time.h>
 #include <string>
 #include <minisat/core/Solver.h>
 #include <minisat/core/SolverTypes.h>
@@ -16,7 +17,7 @@
 
 #define INFINITY 200000
 using namespace std;
-
+using namespace Minisat;
 
 class checkInput
 {
@@ -44,8 +45,8 @@ vector<pair<int, int>> edge_Parser(string st)
     {
         smatch s1 = *i;
         smatch s2 = *++i;
-        e.first = stoi(s1.str());
-        e.second = stoi(s2.str());
+        e.first = stoi(s1.str()) - 1;
+        e.second = stoi(s2.str()) - 1;
         edges_Parsed.push_back(e);
         // cout << e.first << " " << e.second<<endl;
     }
@@ -62,16 +63,20 @@ class make_Graph
     vector<std::pair<int,int>> route;
 
     public:
-    vector<int> *adj;
-
-
+    
     make_Graph(int w)
     {
         this->v = w;
         // int v1 = 0;
-        adj = new vector<int>[w];
+        
 
     }
+
+    // void reset()
+    // {
+    //     route.clear();
+    //     v = 0;
+    // }
 
     void add_ToAdj(vector< std::pair<int,int> > e){
         this->route = e;
@@ -84,8 +89,9 @@ class make_Graph
     vector<pair<int,int>> get_route(){
         return route;
     }
-};
 
+    
+};
 
 pthread_t t1, t2, t3, t4, t5, t6, IO_thread;
 make_Graph g(1);
@@ -170,7 +176,7 @@ vector<int> result_cnf, result_3cnf, result_approxVC1, result_approxVC2, result_
 
     bool checkSatisfiability(Minisat::Solver& minisat, int totalPositions, int v, vector<pair<int,int>> route){
         int rowAdj =0;
-        for( rowAdj =0; rowAdj<=v; rowAdj++){
+        for( rowAdj =0; rowAdj<v; rowAdj++){
             for(int currPositions=0;currPositions<totalPositions;currPositions++){
                 minisat.newVar();
             }
@@ -185,7 +191,7 @@ vector<int> result_cnf, result_3cnf, result_approxVC1, result_approxVC2, result_
 
     vector<int> getRoute(Minisat::Solver& minisat, int currVtx, int v){
         vector<int> p;
-        for (int rowAdj = 0; rowAdj <= v; rowAdj++) {
+        for (int rowAdj = 0; rowAdj < v; rowAdj++) {
             int currPositions = 0;
             while (currPositions < currVtx) {
                 if (minisat.modelValue(generateUniqueIndexForBoolVariable(rowAdj, currPositions, currVtx)) == Minisat::toLbool(0)) {
@@ -211,7 +217,8 @@ vector<int> cnf_sat_vc(make_Graph g1)
     }
 
     // int ip[v];
-    int op[v] = {-1};
+    //int op[v] = {-1};
+      std::vector<int> op(v, -1); 
     int currVtx = 0;
     vector<int> finalRoute[v];
     int low = 0, hi = v, mid;
@@ -242,13 +249,222 @@ vector<int> cnf_sat_vc(make_Graph g1)
         return vector<int> {};
 
 }
+   vector<int> pair_to_int_vector(vector<pair<int, int>> pairs) {
+    vector<int> result;
+    for (auto p : pairs) {
+        result.push_back(p.first);
+        result.push_back(p.second);
+    }
+    return result;
+}
+
+vector<int> SATSOLVER(int Vertexes, int no_covers, vector<int> edges) {
+
+    // Create a MiniSat solver instance
+    Solver solve;
+    //cout<<"In vertex cover"<<endl;
+
+    // Adding these conditions :
+
+   // creating Literals 
+    vector<vector<Lit>> matrix(Vertexes);
+    for (int i = 0; i < Vertexes; i++) {
+        for (int j = 0; j < no_covers; j++) {
+            matrix[i].push_back(mkLit(solve.newVar()));
+        }
+    }
+        //At least one vertex is the ith vertex in the vertex cover:
+for (int j = 0; j < no_covers; j++) 
+{
+    vec<Lit> clause;
+    for (int i = 0; i < Vertexes; i++) 
+    {
+        clause.push(matrix[i][j]);
+    }
+
+    int size_clause = clause.size();
+    vector<vec<Lit>> clauses;
+    if(size_clause > 3){
+
+        vec<Lit> new_clause;
+        Lit lit1 = clause[0],lit2 = clause[1];
+        new_clause.push(lit1);
+        new_clause.push(lit2);
+
+        Var new_var = solve.newVar();
+        new_clause.push(mkLit(new_var));
+        solve.addClause(new_clause);
+
+        for(int i = 2; i < size_clause - 2; i++){
+            vec<Lit> more_new_clause;
+            more_new_clause.push(~mkLit(new_var));
+            if(i < size_clause) {
+                more_new_clause.push(clause[i]);
+            }
+            Var new_var2 = solve.newVar();
+            more_new_clause.push(mkLit(new_var2));
+            // clauses.push_back(more_new_clause);
+            solve.addClause(more_new_clause);
+            new_var = new_var2;
+        }
+
+        vec<Lit> final_clause;
+        final_clause.push(~mkLit(new_var));
+        final_clause.push(clause[size_clause - 2]);
+        final_clause.push(clause[size_clause - 1]);
+        // clauses.push_back(final_clause);
+        solve.addClause(final_clause);
+    }
+
+    // for(int i = 0 ; i < clauses.size();i++)   solve.addClause(clauses[i]);
+    else solve.addClause(clause);
+}
+
+
+
+     //No one vertex can appear twice in a vertex cover
+
+ 
+for (int j = 0; j < no_covers - 1; j++) {
+    for (int k = j + 1; k < no_covers; k++) {
+        vec<Lit> clause;
+        for (int i = 0; i < Vertexes; i++) {
+            clause.push(matrix[i][j]);
+            clause.push(matrix[i][k]);
+        }
+
+        int l = 0, r = Vertexes - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            bool found_j = sign(matrix[mid][j]);
+            bool found_k = sign(matrix[mid][k]);
+            if (!found_j && !found_k) {
+                l = mid + 1;
+            } else if (found_j && found_k) {
+                r = mid - 1;
+            } else {
+                break;
+            }
+        }
+
+        for (int m = l; m <= r; m++) {
+            solve.addClause(~matrix[m][j], ~matrix[m][k]);
+        }
+    }
+}
+
+
+    //No more than one vertex appears in the mth position of the vertex cover
+
+    for (int j = 0; j < no_covers; j++) {
+        for (int i = 0; i < Vertexes - 1; i++) {
+            for (int k = i + 1; k < Vertexes; k++) {
+                solve.addClause(~matrix[i][j], ~matrix[k][j]);
+            }
+        }
+    }
+    //Every edge is incident to at least one vertex in the vertex cover.
+
+    for (int i = 0; i < edges.size(); i += 2) {
+        int x = edges[i];
+        int y = edges[i+1];
+        vec<Lit> clause;
+        for (int j = 0; j < no_covers; j++) {
+            clause.push(matrix[x][j]);
+            clause.push(matrix[y][j]);
+        }
+
+    int size_clause = clause.size();
+   
+    if(size_clause > 3){
+
+        
+
+        vec<Lit> new_clause;
+        Lit lit1 = clause[0],lit2 = clause[1];
+        new_clause.push(lit1);
+        new_clause.push(lit2);
+
+        Var new_var = solve.newVar();
+        new_clause.push(mkLit(new_var));
+        solve.addClause(new_clause);
+
+        for(int i = 2; i < size_clause - 2; i++){
+            vec<Lit> more_new_clause;
+            more_new_clause.push(~mkLit(new_var));
+            if(i < size_clause) {
+                more_new_clause.push(clause[i]);
+            }
+            Var new_var2 = solve.newVar();
+            more_new_clause.push(mkLit(new_var2));
+            solve.addClause(more_new_clause);
+            new_var = new_var2;
+        }
+
+        vec<Lit> final_clause;
+        final_clause.push(~mkLit(new_var));
+        final_clause.push(clause[size_clause - 2]);
+        final_clause.push(clause[size_clause - 1]);
+        solve.addClause(final_clause);
+    }
+
+        else solve.addClause(clause);
+    }
+
+    // Solve the problem using MiniSat
+    bool sat = solve.solve();
+
+    if (sat== true) {
+        // Extract the vertices that are included in the cover
+        vector<int> result;
+        for (int i = 0; i < Vertexes; i++) {
+            for (int j = 0; j < no_covers; j++) {
+                if (toInt(solve.modelValue(matrix[i][j])) == 0) {
+                    result.push_back(i);
+                    break;
+                }
+            }
+        }
+       // cout<<"result calculated"<<endl;
+        return result;
+    } else {
+        // The problem is unsatisfiable
+        return {-1};
+    }
+}
+
+
+
+
+
+
+
 
 // Function to implement CNF-3-SAT-VC
 vector<int> cnf_3_sat_vc(make_Graph g1)
 {
-    // int a = 15;
-    // int b = 20;
-    // return a + b;
+    int V = g1.get_vtx();
+    vector< pair<int,int> > route = g1.get_route();
+    int start = 1;
+int end = V;
+vector<int> answer;
+vector<int> answer_final;
+//vector<int> intVertices{ 0,4,4,1,0,3,3,4,3,2,1,3 };
+vector<int> temp = {-1};
+vector<int> Vertices=  pair_to_int_vector(route);
+for (int i = start; i <= end; i++) {
+    answer = SATSOLVER(V, i , Vertices);
+    //bool check = std::equal(result.begin(), result.end(), tmp.begin());
+    bool cond= (answer == temp);
+    if ( cond == false) {
+        answer_final.clear();
+        answer_final = answer;
+        break;
+    } else {
+        answer_final = answer;
+    }
+}
+sort(answer_final.begin(), answer_final.end(), [](int a, int b) { return a < b;});
     return vector<int> {};
 }
 
@@ -435,7 +651,8 @@ vector<int> refined_approx_vc_2(make_Graph g1)
 
 void print_value(string n, vector<int> result) {
         // cout << "ddddddddddddddddddddddddddccccccccccccajfhs" <<endl;
-
+    // int v = g1.get_vtx();
+    // vector< pair<int,int> > route = g1.get_route();
     cout << n << ": ";
     if (result.empty()) {
         cout << endl;
@@ -448,11 +665,13 @@ void print_value(string n, vector<int> result) {
         // cout << "ajfhs" <<endl;
         int j = i + 1;
         if (j == (int)result.size()) {
-            cout << result[i];
+            cout << (result[i] + 1);
         } else {
-            cout << result[i] << ",";
+            cout << (result[i] + 1) << ",";
         }
     }
+    result.clear();
+    // cout << "V: "<<v<<endl;
     cout << endl;
 }
 
@@ -473,13 +692,33 @@ void *cnf_sat_vc_handler(void *arg)
     return NULL;
 }
 
+
+
+
 // Handler function for CNF-3-SAT-VC
 void *cnf_3_sat_vc_handler(void *arg)
-{
+{   clock_t start, finish;
+	double duration;
+
+	start = clock();
+    result_cnf = cnf_sat_vc(g);
+    print_value("CNF-3-SAT-VC",result_cnf);
+    finish = clock();
+
+	duration = (double)(finish - start)/CLOCKS_PER_SEC;
+	cout << "the duration time is: " << duration << endl;
+
+
+
      result_3cnf = cnf_3_sat_vc(g);
     
     return NULL;
 }
+
+
+
+
+
 
 // Handler function for APPROX-VC-1
 void *approx_vc_1_handler(void *arg)
@@ -546,23 +785,20 @@ void *refined_approx_vc_2_handler(void *arg)
 // Handler function for input/output
 void *IOHandler_thread(void *args)
 {
+    string input;
+    getline(cin, input);
 
-    char cmnd;
-    string lines = "";
+    char cmnd = input[0];
+    string rest = input.substr(2);
+
     int vtx = 0;
-    // int source_vtx = 0;
-    int check_vtx = 0;
-    // int destination_vtx = 0;
-    // make_Graph *g = new make_Graph(0);
+    string edges;
 
-    while (cin >> cmnd)
+    while (true)
     {
-        switch (cmnd)
+        if (cmnd == 'V')
         {
-        case 'V':
-        {
-            cin >> vtx;
-            cin.ignore();
+            vtx = stoi(rest);
             if (vtx < 0)
             {
                 int cmd = 0;
@@ -574,31 +810,46 @@ void *IOHandler_thread(void *args)
                 // delete g;
                 g = make_Graph(vtx);
             }
-
-            break;
         }
-        case 'E':
+        else if (cmnd == 'E')
         {
-            getline(cin, lines);
-            vector<pair<int, int>> line = edge_Parser(lines);
+            edges = rest;
+            vector<pair<int, int>> line = edge_Parser(edges);
             checkInput checker;
-            int check = 0;
+            // cout << vtx <<endl; //5
             if (!checker.edgeValidation(line, vtx))
             {
-                // cout << vtx;
-                cerr << "Error: Invalid vertex entered, the vertices should be between 0 and " << vtx - 1 << endl;
+                cerr << "Error: Invalid vertex entered, the vertices should be between 1 and " << vtx << endl;
                 continue;
             }
             else
             {
-                // int edge1 = 0;
                 g.add_ToAdj(line);
-                // g->getVertexCover();
-
                 pthread_create(&t1, NULL, &cnf_sat_vc_handler, NULL);
-                pthread_join(t1, NULL);
+                // pthread_join(t1, NULL);
+                struct timespec tsp;
+                int t;
+                if (clock_gettime(CLOCK_REALTIME, &tsp) == -1) {
+                    cout<<"Error: in getting current time";
+                }
+                tsp.tv_sec += 120;
+
+                t = pthread_timedjoin_np(t1, NULL, &tsp);
+                if (t != 0) {
+                    cout<<"CNF-SAT-VC: timeout"<<endl;
+                }
                 pthread_create(&t2, NULL, &cnf_3_sat_vc_handler, NULL);
-                pthread_join(t2, NULL);
+                // pthread_join(t2, NULL);
+                int s;
+                if (clock_gettime(CLOCK_REALTIME, &tsp) == -1) {
+                    cout<<"Error: in getting current time";
+                }
+                tsp.tv_sec += 120;
+
+                s = pthread_timedjoin_np(t2, NULL, &tsp);
+                if (s != 0) {
+                    cout<<"CNF-3-SAT-VC: timeout"<<endl;
+                }
                 pthread_create(&t3, NULL, &approx_vc_1_handler, NULL);
                 pthread_join(t3, NULL);
                 pthread_create(&t4, NULL, &approx_vc_2_handler, NULL);
@@ -607,19 +858,21 @@ void *IOHandler_thread(void *args)
                 pthread_join(t5, NULL);
                 pthread_create(&t6, NULL, &refined_approx_vc_2_handler, NULL);
                 pthread_join(t6, NULL);
-                cin.ignore();
             }
-            // shortestroute();
+        }
+        if (!getline(cin, input)) break;
 
-            break;
-        }
-        default:
-            break;
-        }
+        cmnd = input[0];
+        rest = input.substr(2);
     }
-    // delete g;
+
     return NULL;
 }
+
+
+
+
+
 
 int main()
 {
@@ -630,5 +883,7 @@ int main()
         cerr << "Error: thread not created" << endl;
     }
     pthread_join(IO_thread, NULL);
+    // cout<<"I am here";
     pthread_exit(NULL);
-}
+    return 0;
+}   
